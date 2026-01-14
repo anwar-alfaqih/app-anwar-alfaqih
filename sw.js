11927940e0937c21128e72474e1fdc87cf087695
anwar-alfaqih <anwaralfaqih3@gmail.com>
2026-01-15 00:42:19 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,9 @@ const urlsToCache = [
   'index.html',
   'manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700&display=swap'
+  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700&display=swap', 
+
+  'icons/icon-512.png'   
 
 ];
 
